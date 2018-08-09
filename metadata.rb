29d2011cb89cee4e4298de65b8261cd4d65f4755
root <root@chef-workstation.dexter.com.br>
2018-08-09 11:38:48 -0300

    Configuracao Base de Usuarios, Pacotes e Hosts

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'confbase-sowti'
+maintainer 'Henrique do Prado Cezar'
+maintainer_email 'sowti.solucoes@gmail.com'
+license 'All Rights Reserved'
+description 'Configuracao base'
+long_description 'Configuracao base de usuarios, pacotes e hosts'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url "https://github.com/SowTi/confbase-sowti"
